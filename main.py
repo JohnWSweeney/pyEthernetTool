e7493e4b11702d7d8041b1b739d01fe3b7afd98d
JohnWSweeney <JohnWSweeney@gmail.com>
2024-08-11 12:22:17 -0700

    Add graceful socket closure to server (recv data).

--- main.py
+++ main.py
@@ -2,27 +2,38 @@
 import time
 import socket
 
-# def server():
-# 	print("server started")
-# 	HOST = "127.0.0.1"
-# 	PORT = 1234
-# 	buf = 20
+def server(ipAddr, port):
+	print("server started")
+	buf = 100
 
-# 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 	server.bind((HOST, PORT))
-# 	server.listen(1)
-# 	print("listening")
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((ipAddr, port))
+	server.listen(1)
+	print("listening")
 
-# 	while True:
-# 		conn, addr = server.accept()
-# 		print('address: ', addr)
-# 		data = conn.recv(buf)
-# 		if data:
-# 			print("data: ", data)
+	try:
+		while True:
+			conn, addr = server.accept()
+			print('address: ', addr)
+			data = conn.recv(buf)
+			stringData = data.decode('utf-8')
+			if data:
+				if(stringData == "close"):
+					conn.shutdown(socket.SHUT_WR)
+					conn.close()
+					print("socket closed.")
+					break
+				else:
+					print("data:", stringData)
+	except KeyboardInterrupt:
+		print("asdf")
+	
+	server.close()
+	print("server closed.")
 
-def client(host, port, msg):
+def client(ipAddr, port, msg):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host, port))
+	s.connect((ipAddr, port))
 	try:
 		s.send(msg.encode('utf-8'))
 	except:
@@ -31,11 +42,14 @@ def client(host, port, msg):
 
 def main():
 	print("pyEthernetTool")
-	# server()
-	host = "127.0.0.1"
+	# server:
+	ipAddr = "127.0.0.1"
 	port = 1234
-	msg = "hellyeah"
-	client(host, port, msg)
+	server(ipAddr, port)
+
+	# client:
+	# msg = "hellyeah"
+	# client(ipAddr, port, msg)
 
 if __name__ == "__main__":
 	main()
